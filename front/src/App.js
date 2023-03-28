a4013bff3afbe5d3802f25290013c3a80a5b3d75
Sophcoll <107028500+Sophcoll@users.noreply.github.com>
2023-03-29 09:25:49 +1300

    removed double up 'listing' word from listing routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages and components
-import Home from "./pages/Home/Home";
-import Navbar from "./components/navbar/Navbar"
-import Signup from "./pages/Signup/Signup";
-import Login from "./pages/Login/Login";
-import AddListing from "./pages/AddListing/AddListing";
-import NotFound from "./pages/errors/NotFound";
+import Home from './pages/Home/Home';
+import Navbar from './components/navbar/Navbar';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
+import AddListing from './pages/AddListing/AddListing';
+import NotFound from './pages/errors/NotFound';
 
 function App() {
   return (
