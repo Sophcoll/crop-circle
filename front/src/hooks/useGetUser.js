e4b1b3f4cf6adde48c4fb399685d0c4a39641f48
lanaball <allana.m.kennedy@gmail.com>
2023-04-05 10:08:42 +1200

    started abstracts and displaying image

---
+++ front/src/hooks/useGetUser.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export const useGetUser = () => {
+
+ const [ firstName, setFirstName ] = useState()
+ const [ lastName, setLastName ] = useState()
+ const [ email, setEmail ] = useState()
+ const [ ID, setID] = useState()
+
+ const userDetails = (userEmail) => {
+
+  const getUserDetails = async () => {
+  const response = await fetch(`/users/${userEmail}`, {method: "GET"})
+  const userResponse = await response.json()
+
+  const user = userResponse[0]
+  
+  setFirstName(user.firstName)
+  setLastName(user.lastName)
+  setEmail(user.email)
+  setID(user._id)
+  }
+
+  getUserDetails()
+ 
+ }
+
+ return { userDetails, firstName, lastName, email, ID }
+
+}
